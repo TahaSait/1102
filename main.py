0ab3a66da085fe48dfcc609af78258ce6352a889
TahaSait <148285176+TahaSait@users.noreply.github.com>
2023-11-06 12:33:06 -0700

    found missing numbers

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ numbers = open("three_digit_numbers.txt", "r")
 sorted_nums = open("sorted_numbers.txt", "w")
 num = numbers.read()
 nums = num.split()
+for count in range(300, 501):
+    if str(count) not in nums:
+        missin = (f"{count} is missing\n")
+        print(missin)
 sort_nums = str(sorted(nums))
 sorted_nums.write(sort_nums)
 numbers.close()
